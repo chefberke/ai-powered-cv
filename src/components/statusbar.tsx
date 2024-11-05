50239145f0547f25cf595f416e9aea3360ecc736
Berke Kanber <berkekanber@gmail.com>
2024-11-05 14:32:56 +0300

    refactor: statusbar color

--- src/components/statusbar.tsx
+++ src/components/statusbar.tsx
@@ -5,10 +5,10 @@ function statusbar() {
   return (
     <div>
       <div className="flex items-center justify-center gap-2">
-        <div className="bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 w-24 h-3 rounded-full"></div>
-        <div className="bg-gray-200 w-24 h-3 rounded-full"></div>
-        <div className="bg-gray-200 w-24 h-3 rounded-full"></div>
-        <div className="bg-gray-200 w-24 h-3 rounded-full"></div>
+        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-24 h-2 rounded-full"></div>
+        <div className="bg-gray-200 w-24 h-2 rounded-full"></div>
+        <div className="bg-gray-200 w-24 h-2 rounded-full"></div>
+        <div className="bg-gray-200 w-24 h-2 rounded-full"></div>
       </div>
       <div className="mt-3">
         <h2 className="text-sm text-black flex items-center gap-1">
